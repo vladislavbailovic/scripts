aa0643297e75edde181681a52f64dba665359ece
vladislav <ve@incsub.com>
2016-12-13 16:52:41 +0100

    Convert internally used image format to PNG

--- gipsum.sh
+++ gipsum.sh
@@ -8,11 +8,10 @@ SIZE="$HSIZE"'x'"$VSIZE"
 
 # Define colors, say 100
 for i in $(seq 1 100); do
-	tmp='rgb('
-	tmp="$tmp"$(shuf -i1-255 -n 1)","
-	tmp="$tmp"$(shuf -i1-255 -n 1)","
-	tmp="$tmp"$(shuf -i1-255 -n 1)")"
-	COLORS[$i]="$tmp" 
+	R=$(shuf -i1-255 -n 1)
+	G=$(shuf -i1-255 -n 1)
+	B=$(shuf -i1-255 -n 1)
+	COLORS[$i]="rgb($R,$G,$B)" 
 done;
 
 COL1IDX="$(shuf -i1-100 -n1)"
@@ -22,12 +21,14 @@ COL2IDX="$(shuf -i1-100 -n1)"
 COL2="${COLORS[$COL2IDX]}"
 
 COLRANGE="$COL1"'-'"$COL2"
+FORMAT="png"
 
 PIXELATE="no"
 
-COMMAND="convert -size '$SIZE' 'plasma:$COLRANGE' 'jpeg:-'"
+OPTIMIZATION="-quality 95 -depth 8"
+COMMAND="convert -size '$SIZE' 'plasma:$COLRANGE' $OPTIMIZATION '$FORMAT:-'"
 if [ "yes" == "$PIXELATED" ]; then
-	COMMAND="$COMMAND | convert -scale '5%' -scale '$SIZE'! 'jpeg:-' 'jpeg:-'"
+	COMMAND="$COMMAND | convert -scale '5%' -scale '$SIZE'! '$FORMAT:-' '$FORMAT:-'"
 fi
 
 eval $COMMAND
