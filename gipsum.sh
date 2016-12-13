1edf19ce0ab830590138d44831ae3503bd6737d2
vladislav <ve@incsub.com>
2016-12-13 17:06:02 +0100

    Started on parsing the command line options

--- gipsum.sh
+++ gipsum.sh
@@ -4,7 +4,6 @@
 
 HSIZE=200
 VSIZE=100
-SIZE="$HSIZE"'x'"$VSIZE"
 
 # Define colors, say 100
 for i in $(seq 1 100); do
@@ -21,14 +20,46 @@ COL2IDX="$(shuf -i1-100 -n1)"
 COL2="${COLORS[$COL2IDX]}"
 
 COLRANGE="$COL1"'-'"$COL2"
-FORMAT="png"
 
+FORMAT="png"
 PIXELATE="no"
 
-OPTIMIZATION="-quality 95 -depth 8"
+while getopts "t:pow:h:s:f:" opt; do
+	case $opt in
+		t)
+			FORMAT="$OPTARG"
+			;;
+		p)
+			PIXELATE="yes"
+			;;
+		o)
+			OPTIMIZATION="-quality 95 -depth 8"
+			;;
+		w)
+			HSIZE="$OPTARG"
+			;;
+		h)
+			VSIZE="$OPTARG"
+			;;
+		s)
+			HSIZE="${OPTARG%%x*}"
+			VSIZE="${OPTARG##*x}"
+			;;
+		f)
+			FILENAME="$OPTARG"
+			;;
+	esac
+done
+SIZE="$HSIZE"'x'"$VSIZE"
+
 COMMAND="convert -size '$SIZE' 'plasma:$COLRANGE' $OPTIMIZATION '$FORMAT:-'"
 if [ "yes" == "$PIXELATED" ]; then
 	COMMAND="$COMMAND | convert -scale '5%' -scale '$SIZE'! '$FORMAT:-' '$FORMAT:-'"
 fi
+if [ -n "$FILENAME" ]; then
+	COMMAND="$COMMAND > $FILENAME"
+fi
+
+echo "$COMMAND"; exit
 
 eval $COMMAND
